120d993f51e88d4d70e25f7618f3ef8fd33e39d6
been2zzz <been2zz@naver.com>
2021-09-21 17:26:36 +0900

    change img code

--- js/background.js
+++ js/background.js
@@ -5,6 +5,6 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
