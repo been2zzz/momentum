0c23e2083d64ec13a0ffedd9201132ad9f09022b
devGO <been2zz@naver.com>
2021-10-09 20:14:53 +0900

    CSS 적용

--- js/background.js
+++ js/background.js
@@ -6,5 +6,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = 'bgImage'
 
 document.body.appendChild(bgImage);
